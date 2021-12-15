79ade53b5a326cdb7dff38c4aec38cfb49729fa2
Ольга Пинчук <pinchuk759@gmail.com>
2021-12-15 22:19:46 +0300

    lesson 8 доделать

---
+++ src/main/java/baseEntities/BaseTest.java
@@ -0,0 +1,49 @@
+package baseEntities;
+
+
+import com.tms.core.ReadProperties;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.apache.hc.core5.http.io.SessionOutputBuffer;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+    public class BaseTest {
+        protected WebDriver driver;
+
+        @BeforeMethod
+        public void setUp(){
+            switch (ReadProperties.getBrowserType().toLowerCase()){
+                case "chrome":
+                    WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+                    ChromeOptions chromeOptions= new ChromeOptions();
+                    chromeOptions.addArguments("--disable-gpu");
+                    chromeOptions.addArguments("--silent");
+                    chromeOptions.setHeadless(ReadProperties.getHeadless());
+                    //перенабрать
+                    driver=new ChromeDriver(chromeOptions);
+
+                    break;
+                case "firefox":
+                    WebDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    System.out.println("данный браузер не поддерживается");
+                    break;
+            }
+            driver.manage().window().maximize();
+            driver.get(ReadProperties.getUrl());
+
+        }
+
+        @AfterMethod
+        public void tearDown(){
+            driver.quit();
+        }
+    }
