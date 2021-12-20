aa2c01de75e6c070cf0b80594773ef311aa8ed13
Ольга Пинчук <pinchuk759@gmail.com>
2021-12-20 20:25:30 +0300

    Часть1

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.*;
+
+public class BaseTest {
+        // Before annotations
+        @BeforeTest
+        public void beforeTest(){ System.out.println("BeforeTest"); }
+
+        @BeforeSuite
+        public void beforeSuite(){ System.out.println("BeforeSuite"); }
+
+        @BeforeGroups
+        public void beforeGroups(){ System.out.println("BeforeGroups"); }
+
+        @BeforeClass
+        public void beforeClass(){ System.out.println("BeforeClass"); }
+
+        @BeforeMethod
+        public void beforeMethod(){ System.out.println("BeforeMethod"); }
+
+        // After annotations
+        @AfterTest
+        public void afterTest(){ System.out.println("AfterTest"); }
+
+        @AfterSuite
+        public void afterSuite(){ System.out.println("AfterSuite"); }
+
+        @AfterGroups
+        public void afterGroups(){ System.out.println("AfterGroups"); }
+
+        @AfterClass
+        public void afterClass(){ System.out.println("AfterClass"); }
+
+        @AfterMethod
+        public void afterMethod(){ System.out.println("AfterMethod"); }
+
+    }
+
---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,10 @@
+
+import org.testng.annotations.Test;
+
+public class CalcTest extends BaseTest{
+
+    @Test
+    public void testSum(){
+        System.out.println("CalcTest -> testSum");
+    }
+}
