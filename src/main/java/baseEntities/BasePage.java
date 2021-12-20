8db4ef8a523f1f31975e0fb103cbe8ce261c547f
Ольга Пинчук <pinchuk759@gmail.com>
2021-12-20 20:35:23 +0300

    Работа в классе

--- src/main/java/baseEntities/BasePage.java
+++ src/main/java/baseEntities/BasePage.java
@@ -14,6 +14,6 @@ public class BasePage {
         try{
         return driver.findElement(by).isDisplayed();
     }catch (NoSuchElementException nsex){
-        return false}
+        return false;}
     }
 }
