5e62efde78c63f8e5c680b49a323d731f9e16419
Ольга Пинчук <pinchuk759@gmail.com>
2022-01-08 15:15:28 +0300

    изменения

--- src/main/java/utils/Waits.java
+++ src/main/java/utils/Waits.java
@@ -3,6 +3,7 @@
 package utils;
 
 import com.tms.core.ReadProperties;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -24,5 +25,8 @@ public final class Waits {
 
     public boolean waitForVisibility(WebElement element){
         return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+    }
+    public WebElement waitForVisibility(By by){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
 }
--- src/test/java/tests/WaitsTest.java
+++ src/test/java/tests/WaitsTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.Waits;
 
 public class WaitsTest extends BaseTest {
 
@@ -12,10 +13,14 @@ public class WaitsTest extends BaseTest {
     public void explicitlyWait(){
         driver.get("http://the-internet.herokuapp.com/hovers");
 
+        Waits waits= new Waits(driver);
+
         WebElement figure = driver.findElement(By.className("figure"));
         figure.click();
-
-        WebElement caption = driver.findElement(By.xpath("//h5[. = 'name: user1']"));
-        Assert.assertTrue(caption.isDisplayed());
+//Для невидимых элементов
+        /*WebElement caption = driver.findElement(By.xpath("//h5[. = 'name: user1']"));
+        Assert.assertTrue(caption.isDisplayed());*/
+        //Для видимых элементов
+        Assert.assertTrue(waits.waitForVisibility(By.xpath("//h5[. = 'name: user1']")).isDisplayed());
     }
 }
