276235317011b6d57c111cd56fd220fbbb6816c7
Ольга Пинчук <pinchuk759@gmail.com>
2022-01-16 03:14:51 +0300

    запись занятия

--- src/test/java/tests/ActionTest.java
+++ src/test/java/tests/ActionTest.java
@@ -4,6 +4,7 @@ import baseEntities.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.WeakHashMap;
--- src/test/java/tests/JSTest.java
+++ src/test/java/tests/JSTest.java
@@ -5,15 +5,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class JSTest extends BaseTest {
     @Test
-    public void jsTest () throws InterruptedException {
-        driver.get("");
-        WebElement webElement = driver.findElement(By.id(""));
+    public void jsTest() throws InterruptedException {
+        driver.get("http://the-internet.herokuapp.com/jqueryui/menu#");
+
+        WebElement webElement = driver.findElement(By.id("ui-id-2"));
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
         javascriptExecutor.executeScript("arguments[0].click();", webElement);
-        Thread.sleep(1000);
+
+        Thread.sleep(5000);
     }
 }
