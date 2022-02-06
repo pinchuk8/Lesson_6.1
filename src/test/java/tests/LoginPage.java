995a4b4040708dd489c39ccefb007c8fc9ea2be5
user <pinchuk759@gmail.com>
2022-02-06 17:04:24 +0300

    selenide

---
+++ src/test/java/tests/LoginPage.java
@@ -0,0 +1,21 @@
+package tests;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {  private final By username_selector = By.id("name");
+    private final String password_selector = "#password";
+    private final String login_button_selector = "#button_primary";
+
+    public SelenideElement getUsernameField() {
+        return $(username_selector);//$ открывает браузер
+    }
+    public SelenideElement getPasswordField() {
+        return $(password_selector);
+    }
+    public SelenideElement getLoginButton() {
+        return $(login_button_selector);
+    }
+}
